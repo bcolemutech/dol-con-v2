f1c0964af5f22012bca5e7fc423b155348cec382
bcolemutech <bcolemutech@hotmail.com>
2023-05-07 00:36:55 -0500

    Remove notes and vertices

--- DolSdk/BaseTypes/Map.cs
+++ DolSdk/BaseTypes/Map.cs
@@ -6,8 +6,6 @@ public class Map
     public Settings settings { get; set; }
     public Coords coords { get; set; }
     public Cell cells { get; set; }
-    public List<Vertex> vertices { get; set; }
     public Biomes biomes { get; set; }
-    public List<Note> notes { get; set; }
     public List<NameBasis> nameBases { get; set; }
 }
