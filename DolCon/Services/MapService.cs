7958a3d2c9d51fe79621784148e93caacaeb1e2f
bcolemutech <bcolemutech@hotmail.com>
2023-05-07 01:31:09 -0500

    Scan for largest city and set indicator

--- DolCon/Services/MapService.cs
+++ DolCon/Services/MapService.cs
@@ -32,7 +32,7 @@ public class MapService : IMapService
         }
 
         AnsiConsole.MarkupLine("[red bold]No maps found![/]");
-        AnsiConsole.WriteLine("Create a map using Azgaars Fantasy Map Generator (https://azgaar.github.io/Fantasy-Map-Generator/).");
+        AnsiConsole.WriteLine("Create a map using Azgaar's Fantasy Map Generator (https://azgaar.github.io/Fantasy-Map-Generator/).");
         AnsiConsole.WriteLine("Once you have created a map, export it as a JSON file and place it in the maps directory.");
         
         return new List<FileInfo>();
@@ -40,15 +40,28 @@ public class MapService : IMapService
 
     public async Task<Map> LoadMap(FileInfo mapFile)
     {
-        Map? map = null;
+        Map map = new();
         await AnsiConsole.Status().StartAsync("Loading map...", async ctx =>
         {
             ctx.Spinner(Spinner.Known.Star);
             ctx.SpinnerStyle(Style.Parse("yellow"));
-            var stream = File.OpenRead(mapFile.FullName);
-            map = await JsonSerializer.DeserializeAsync<Map>(stream);
+            ctx.Refresh();
+            map = await DeserializeMap(mapFile) ?? throw new DolMapException("Failed to load map");
+            AnsiConsole.MarkupLine("Loaded map [yellow]{0}[/]", mapFile.Name);
+            ctx.Status("Identifying City of Light...");
+            ctx.Refresh();
+
+            var topPop = map.cells.burgs.Max(x => x.population);
+            var cityOfLight = map.cells.burgs.First(x => Math.Abs(x.population - topPop) < 0.01);
+            cityOfLight.isCityOfLight = true;
         });
-        return map ?? throw new DolMapException("Failed to load map");
+        return map;
+    }
+
+    private static async Task<Map?> DeserializeMap(FileSystemInfo mapFile)
+    {
+        var stream = File.OpenRead(mapFile.FullName);
+        return await JsonSerializer.DeserializeAsync<Map>(stream);
     }
 }
 
