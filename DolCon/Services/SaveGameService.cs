b7786cf2baf875af428b01336f057442c9937ce2
bcolemutech <bcolemutech@hotmail.com>
2023-05-06 22:35:12 -0500

    - Save game service - create settings if it doesn't exist

---
+++ DolCon/Services/SaveGameService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using DolCon.Services;
+using Spectre.Console;
+
+public interface ISaveGameService
+{
+}
+
+public class SaveGameService : ISaveGameService
+{
+    private readonly Settings? _settings;
+    private readonly string _settingsPath;
+    
+    public SaveGameService()
+    {
+        _settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DolCon", "settings.json");
+        AnsiConsole.WriteLine("Settings path: [yellow]{0}[/]", _settingsPath);
+        if (File.Exists(_settingsPath))
+        {
+            AnsiConsole.WriteLine("Loading settings...");
+            _settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(_settingsPath));
+        }
+        else
+        {
+            AnsiConsole.WriteLine("Settings not found, creating new settings...");
+            _settings = new Settings();
+            File.WriteAllText(_settingsPath, JsonSerializer.Serialize(_settings));
+        }
+    }
+}
