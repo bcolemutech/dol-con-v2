ddfae3fcc5a489cb3122c7c5ec1637b5a8cefe66
bcolemutech <bcolemutech@hotmail.com>
2023-05-06 22:34:09 -0500

    Inject services and start app

--- DolCon/Program.cs
+++ DolCon/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DolCon;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddSingleton<ISaveGameService, SaveGameService>();
+        services.AddHostedService<MainMenu>();
+    })
+    .Build();
+    
+await host.RunAsync();
