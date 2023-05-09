459bc7b61966abd7bbc79d84d5a2e080a687cb3b
bcolemutech <bcolemutech@hotmail.com>
2023-05-09 00:00:42 -0500

    Add province type

--- DolCon/Models/BaseTypes/Map.cs
+++ DolCon/Models/BaseTypes/Map.cs
@@ -1,13 +1,30 @@
 ﻿namespace DolCon.Models.BaseTypes;
 
+using System.Text.Json.Serialization;
+
 public class Map
 {
     public Info info { get; set; }
     public Settings settings { get; set; }
     public Coords coords { get; set; }
-    public Cell cells { get; set; }
+    
+    [JsonPropertyName("cells")]
+    public MapCollections Collections { get; set; }
     public Biomes biomes { get; set; }
     public List<NameBasis> nameBases { get; set; }
     public Party Party { get; set; }
     public Guid CurrentPlayerId { get; set; }
+}
+
+public class MapCollections
+{
+    public List<Cell> cells { get; set; }
+    public List<object> features { get; set; }
+    public List<Culture> cultures { get; set; }
+    public List<Burg> burgs { get; set; }
+    public List<State> states { get; set; }
+    public List<Province> provinces { get; set; }
+    public List<Religion> religions { get; set; }
+    public List<River> rivers { get; set; }
+    public List<Marker> markers { get; set; }
 }
