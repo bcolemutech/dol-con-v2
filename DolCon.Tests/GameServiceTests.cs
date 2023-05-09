e0469066935a8810f352bdf3b24b2ea2d0615db5
bcolemutech <bcolemutech@hotmail.com>
2023-05-08 21:19:29 -0500

    Tests for parsing screen from key stroke

---
+++ DolCon.Tests/GameServiceTests.cs
@@ -0,0 +1,55 @@
+namespace DolCon.Tests;
+
+using Enums;
+using FluentAssertions;
+
+public class GameServiceTests
+{
+    [Fact]
+    public void EscapeKeyParsesToExitScreen()
+    {
+        var result = (Screen)ConsoleKey.Escape;
+
+        result.Should().Be(Screen.Exit);
+    }
+    
+    [Fact]
+    public void HKeyParsesToHomeScreen()
+    {
+        var result = (Screen)ConsoleKey.H;
+
+        result.Should().Be(Screen.Home);
+    }
+    
+    [Fact]
+    public void NKeyParsesToNavigationScreen()
+    {
+        var result = (Screen)ConsoleKey.N;
+
+        result.Should().Be(Screen.Navigation);
+    }
+    
+    [Fact]
+    public void IKeyParsesToInventoryScreen()
+    {
+        var result = (Screen)ConsoleKey.I;
+
+        result.Should().Be(Screen.Inventory);
+    }
+    
+    [Fact]
+    public void CKeyParsesToCharacterScreen()
+    {
+        var result = (Screen)ConsoleKey.C;
+
+        result.Should().Be(Screen.Character);
+    }
+    
+    [Fact]
+    public void QKeyParsesToQuestsScreen()
+    {
+        var result = (Screen)ConsoleKey.Q;
+
+        result.Should().Be(Screen.Quests);
+    }
+}
