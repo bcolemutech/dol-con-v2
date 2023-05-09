c95ef4ad29cb34ab3efb198b06b7ab1a15f9249c
bcolemutech <bcolemutech@hotmail.com>
2023-05-09 00:15:52 -0500

    Populate all fields appropriately for area and burg

--- DolCon/Views/GameService.Home.cs
+++ DolCon/Views/GameService.Home.cs
@@ -2,21 +2,63 @@
 
 using Services;
 using Spectre.Console;
+using Spectre.Console.Rendering;
 
 public partial class GameService
 {
     private void RenderHome()
     {
-        var currentCell = SaveGameService.CurrentMap.cells.cells.First(x => x.i == SaveGameService.Party.Cell);
-        var burg = SaveGameService.CurrentMap.cells.burgs.FirstOrDefault(x => x.i == SaveGameService.Party.Burg);
+        var currentCell = SaveGameService.CurrentMap.Collections.cells.First(x => x.i == SaveGameService.Party.Cell);
+        var burg = SaveGameService.Party.Burg.HasValue
+            ? SaveGameService.CurrentMap.Collections.burgs[SaveGameService.Party.Burg.Value]
+            : null;
         var biome = SaveGameService.CurrentMap.biomes.name[currentCell.biome];
+        var province = SaveGameService.CurrentMap.Collections.provinces[currentCell.province];
+        var state = SaveGameService.CurrentMap.Collections.states[currentCell.state];
+        var homePanels = new List<IRenderable>
+        {
+            new Panel(
+                Align.Center(
+                    new Rows(
+                        new Markup("[bold]Current Area[/]"),
+                        new Markup($"Biome: [green]{biome}[/]"),
+                        new Markup($"Rural Population: [green]{Convert.ToInt32(currentCell.pop * 1000)}[/]"),
+                        new Markup($"Province: [green]{province.fullName}[/]"),
+                        new Markup($"State: [green]{state.fullName}[/]")
+                    ),
+                    VerticalAlignment.Middle))
+        };
+        if (burg != null)
+        {
+            var cityTitle = burg.isCityOfLight
+                ? $"[rapidblink yellow Underline]The City of Light - {burg.name}[/]"
+                : $"[bold]Current Burg: {burg.name}[/]";
+            var features = new List<string>
+            {
+                $"{(burg.capital == 1 ? "Capitol" : string.Empty)}",
+                $"{(burg.port == 1 ? "Port" : string.Empty)}",
+                $"{(burg.citadel == 1 ? "Castle" : string.Empty)}",
+                $"{(burg.walls == 1 ? "Walls" : string.Empty)}",
+                $"{(burg.plaza == 1 ? "Marketplace" : string.Empty)}",
+                $"{(burg.temple == 1 ? "Temple" : string.Empty)}",
+                $"{(burg.shanty == 1 ? "Shanty Town" : string.Empty)}"
+            };
+            var featuresString = string.Join(" | ", features.Where(x => !string.IsNullOrEmpty(x)));
+            homePanels.Add(new Panel(
+                Align.Center(
+                    new Rows(
+                        new Markup(cityTitle),
+                        new Markup($"Population: [green]{Convert.ToInt32(burg.population * 1000)}[/]"),
+                        new Text(featuresString)
+                    )
+                    , VerticalAlignment.Middle)));
+        }
+
         _display.Update(
             new Panel(
                     Align.Center(
                         new Rows(
-                            new Markup($"Burg name: [green]{burg?.name ?? "None"}[/]"),
-                            new Markup("Biome: [green]" + biome + "[/]"),
-                            new Markup("Population: [green]" + currentCell.pop + "[/]")
+                            homePanels
                         ),
                         VerticalAlignment.Middle))
                 .Expand());
@@ -24,7 +66,11 @@ public partial class GameService
         _controls.Update(
             new Panel(
                     Align.Center(
-                        new Markup("[Green]1[/]: Navigate | [Red]Esc[/]: Exit"), VerticalAlignment.Middle))
+                        new Rows(
+                            new Markup(
+                                "[bold]Standard controls[/]: ([green bold]H[/])ome | ([green bold]N[/])avigation | ([Red bold]E[/])xit")
+                        ),
+                        VerticalAlignment.Middle))
                 .Expand());
     }
 }
