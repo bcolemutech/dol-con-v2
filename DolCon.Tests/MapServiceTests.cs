ed5409e2056f1cc51eee8a046ee6af76aeb0825d
bcolemutech <bcolemutech@hotmail.com>
2023-05-12 00:58:32 -0500

    Map service method to identify direction

---
+++ DolCon.Tests/MapServiceTests.cs
@@ -0,0 +1,20 @@
+﻿namespace DolCon.Tests;
+
+using System.Drawing;
+using Enums;
+using FluentAssertions;
+using Services;
+
+public class MapServiceTests
+{
+    [Fact]
+    public void WhenGetDirectionReturnsProperDirection()
+    {
+        var destination = new Point((int)426.95, (int)82.37);
+        var origin = new Point((int)519.37, (int)164.33);
+
+        var actual = MapService.GetDirection(origin, destination);
+
+        actual.Should().Be(Direction.Northwest);
+    }
+}
