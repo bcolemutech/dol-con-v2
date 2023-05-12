5acfe9f7bd51cb670d669c7a61bc696bbe1e9659
bcolemutech <bcolemutech@hotmail.com>
2023-05-11 21:25:06 -0500

    Test for provinces collection converter

---
+++ DolCon.Tests/JsonConversionTests.cs
@@ -0,0 +1,29 @@
+﻿namespace DolCon.Tests;
+
+using FluentAssertions;
+using Models.BaseTypes;
+using Newtonsoft.Json;
+
+public class JsonConversionTests
+{
+    [Fact]
+    public void CanConvertListWithMultipleTypesToListOfProvinces()
+    {
+        var json = @"
+[
+    0,
+    {""i"":1,""state"":0,""center"":0,""burg"":0,""name"":""test1"",""formName"":"""",""fullName"":"""",""color"":""#61ddb9"",""coa"":{""t1"":"""",""ordinaries"":[],""charges"":[],""shield"":""""}},
+    {""i"":2,""state"":0,""center"":0,""burg"":0,""name"":""test2"",""formName"":"""",""fullName"":"""",""color"":""#61ddb9"",""coa"":{""t1"":"""",""ordinaries"":[],""charges"":[],""shield"":""""}}
+]
+";
+        var converter = new JsonConverter[] { new ProvincesConverter() };
+        List<Province>? provinces = JsonConvert.DeserializeObject<List<Province>>(json, converter);
+
+        provinces.Should().NotBeNull();
+
+        provinces.Should().HaveCount(3);
+        provinces[0].Should().NotBeNull();
+        provinces[0].i.Should().Be(0);
+        provinces[0].name.Should().BeNullOrEmpty();
+    }
+}
