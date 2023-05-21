308ebc4b19574d4ec039118be67ff4fe5e3992b8
bcolemutech <bcolemutech@hotmail.com>
2023-05-21 16:20:27 -0500

    Location class

---
+++ DolCon/Models/Location.cs
@@ -0,0 +1,12 @@
+﻿namespace DolCon.Models;
+
+using Enums;
+
+public class Location
+{
+    string Name { get; set; }
+    LocationType Type { get; set; }
+    Rarity Rarity { get; set; }
+    int Explored { get; set; }
+    DateTime LastExplored { get; set; }
+}
