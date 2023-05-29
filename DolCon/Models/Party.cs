098a0ed244fcad4e91f5a53247413015363883e1
bcolemutech <bcolemutech@hotmail.com>
2023-05-28 20:02:05 -0500

    Add property for current location

--- DolCon/Models/Party.cs
+++ DolCon/Models/Party.cs
@@ -5,4 +5,6 @@ public class Party
     public List<Player> Players { get; set; } = new();
     public int Cell { get; set; }
     public int? Burg { get; set; }
+    
+    public Guid? Location { get; set; }
 }
