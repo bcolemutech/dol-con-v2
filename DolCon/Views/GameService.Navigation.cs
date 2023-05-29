79f530a8edf9306ca8ea05cdd20b6b04e52527ce
bcolemutech <bcolemutech@hotmail.com>
2023-05-28 20:03:22 -0500

    Navigation display and processing

--- DolCon/Views/GameService.Navigation.cs
+++ DolCon/Views/GameService.Navigation.cs
@@ -1,6 +1,6 @@
 ﻿namespace DolCon.Views;
 
-using System.Drawing;
+using Models.BaseTypes;
 using Services;
 using Spectre.Console;
 using Spectre.Console.Rendering;
@@ -8,23 +8,29 @@ using Spectre.Console.Rendering;
 public partial class GameService
 {
     private Dictionary<int, int> _directionOptions = new();
+    private Dictionary<int, Guid> _locationOptions = new();
 
-    private void RenderNavigation(char value)
+    private void RenderNavigation(ConsoleKeyInfo value)
     {
         var currentCell = SaveGameService.CurrentCell;
         var localBurg = currentCell.burg > 0 ? SaveGameService.GetBurg(currentCell.burg) : null;
 
-        if (char.ToLower(value) == 'l' && SaveGameService.Party.Burg != null)
+        if (char.ToLower(value.KeyChar) == 'l' &&
+            (SaveGameService.Party.Burg != null || SaveGameService.Party.Location != null))
         {
-            SaveGameService.Party.Burg = null;
+            SaveGameService.Party.Burg = SaveGameService.Party.Location == null ? null : SaveGameService.Party.Burg;
+            SaveGameService.Party.Location = null;
         }
-
-        if (char.ToLower(value) == 'b' && SaveGameService.CurrentBurg is null && localBurg != null)
+        else if (char.ToLower(value.KeyChar) == 'b' && SaveGameService.CurrentBurg is null && localBurg != null)
         {
             SaveGameService.Party.Burg = localBurg.i;
         }
-
-        if (int.TryParse(value.ToString(), out var direction))
+        else if ((value.Modifiers == ConsoleModifiers.Control || SaveGameService.CurrentBurg is not null) &&
+                 int.TryParse(value.KeyChar.ToString(), out var selection))
+        {
+            SaveGameService.Party.Location = _locationOptions[selection];
+        }
+        else if (int.TryParse(value.KeyChar.ToString(), out var direction))
         {
             SaveGameService.Party.Cell = _directionOptions[direction];
             _imageService.ProcessSvg();
@@ -36,58 +42,11 @@ public partial class GameService
 
         if (burg != null)
         {
-            _display.Update(
-                new Panel(
-                    Align.Center(
-                        new Markup("Burg navigation is not implemented yet!"), VerticalAlignment.Middle
-                    )
-                ).Expand()
-            );
-            _ctx.Refresh();
+            RenderBurgNavigation(burg);
         }
         else
         {
-            var table = new Table();
-
-            _display.Update(
-                new Panel(
-                    new Rows(
-                        Align.Center(
-                            new Markup(
-                                $"Local Burg: [green bold]{(localBurg != null ? localBurg.name : "None")}[/]")),
-                        Align.Center(
-                            table
-                        ))));
-            _ctx.Refresh();
-
-            table.AddColumn(new TableColumn("Key"));
-            table.AddColumn(new TableColumn("Direction"));
-            table.AddColumn(new TableColumn("Province"));
-            table.AddColumn(new TableColumn("State"));
-            table.AddColumn(new TableColumn("Biome"));
-            table.AddColumn(new TableColumn("Burg"));
-            _ctx.Refresh();
-
-            _directionOptions = new Dictionary<int, int>();
-
-            var i = 0;
-
-            foreach (var cellId in currentCell.c)
-            {
-                var cell = SaveGameService.GetCell(cellId);
-                var cellBurg = SaveGameService.GetBurg(cell.burg);
-                var cellBiome = SaveGameService.GetBiome(cell.biome);
-                var cellProvince = SaveGameService.GetProvince(cell.province);
-                var cellState = SaveGameService.GetState(cell.state);
-                var cellDirection = MapService.GetDirection(currentCell.p[0], currentCell.p[1], cell.p[0], cell.p[1]);
-                var key = i++;
-                _directionOptions.Add(key, cellId);
-
-                table.AddRow(key.ToString(), cellDirection.ToString(), cellProvince.fullName,
-                    cellState.fullName ?? string.Empty, cellBiome, cellBurg?.name ?? "None");
-
-                _ctx.Refresh();
-            }
+            RenderCellNavigation(localBurg, currentCell);
         }
 
         var controlLines = new List<IRenderable>
@@ -120,5 +79,112 @@ public partial class GameService
                         VerticalAlignment.Middle))
                 .Expand());
         _ctx.Refresh();
+    }
+
+    private void RenderBurgNavigation(Burg burg)
+    {
+        var locationsTable = new Table();
+
+        _display.Update(
+            new Panel(
+                new Rows(
+                    Align.Center(
+                        new Markup(
+                            $"Current Burg: [green bold]{burg.name}[/]")),
+                    Align.Center(
+                        locationsTable
+                    ))));
+        _ctx.Refresh();
+
+        locationsTable.AddColumn(new TableColumn("Key"));
+        locationsTable.AddColumn(new TableColumn("Location"));
+        locationsTable.AddColumn(new TableColumn("Type"));
+        locationsTable.AddColumn(new TableColumn("Rarity"));
+        _ctx.Refresh();
+
+        _locationOptions = new Dictionary<int, Guid>();
+
+        var i = 0;
+
+        foreach (var location in burg.locations)
+        {
+            _locationOptions.Add(i, location.Id);
+            locationsTable.AddRow(
+                $"{i}",
+                location.Name,
+                location.Type.Type,
+                location.Rarity.ToString());
+            i++;
+            _ctx.Refresh();
+        }
+    }
+
+    private void RenderCellNavigation(Burg? localBurg, Cell currentCell)
+    {
+        var cellsTable = new Table();
+        var locationsTable = new Table();
+
+        _display.Update(
+            new Panel(
+                new Rows(
+                    Align.Center(
+                        new Markup(
+                            $"Local Burg: [green bold]{(localBurg != null ? localBurg.name : "None")}[/]")),
+                    Align.Center(
+                        cellsTable
+                    ),
+                    Align.Center(
+                        locationsTable
+                    ))));
+        _ctx.Refresh();
+
+        cellsTable.AddColumn(new TableColumn("Key"));
+        cellsTable.AddColumn(new TableColumn("Direction"));
+        cellsTable.AddColumn(new TableColumn("Province"));
+        cellsTable.AddColumn(new TableColumn("State"));
+        cellsTable.AddColumn(new TableColumn("Biome"));
+        cellsTable.AddColumn(new TableColumn("Burg"));
+        _ctx.Refresh();
+
+        _directionOptions = new Dictionary<int, int>();
+
+        var i = 0;
+
+        foreach (var cellId in currentCell.c)
+        {
+            var cell = SaveGameService.GetCell(cellId);
+            var cellBurg = SaveGameService.GetBurg(cell.burg);
+            var cellBiome = SaveGameService.GetBiome(cell.biome);
+            var cellProvince = SaveGameService.GetProvince(cell.province);
+            var cellState = SaveGameService.GetState(cell.state);
+            var cellDirection = MapService.GetDirection(currentCell.p[0], currentCell.p[1], cell.p[0], cell.p[1]);
+            var key = i++;
+            _directionOptions.Add(key, cellId);
+
+            cellsTable.AddRow(key.ToString(), cellDirection.ToString(), cellProvince.fullName,
+                cellState.fullName ?? string.Empty, cellBiome, cellBurg?.name ?? "None");
+
+            _ctx.Refresh();
+        }
+
+        locationsTable.AddColumn(new TableColumn("Key"));
+        locationsTable.AddColumn(new TableColumn("Location"));
+        locationsTable.AddColumn(new TableColumn("Type"));
+        locationsTable.AddColumn(new TableColumn("Rarity"));
+        _ctx.Refresh();
+
+        _locationOptions = new Dictionary<int, Guid>();
+
+        i = 0;
+
+        foreach (var location in currentCell.locations)
+        {
+            var key = i++;
+            var keyString = $"Ctrl+{key}";
+            _locationOptions.Add(key, location.Id);
+
+            locationsTable.AddRow(keyString, location.Name, location.Type.Type, location.Rarity.ToString());
+            _ctx.Refresh();
+        }
     }
 }
