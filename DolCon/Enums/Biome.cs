2069b721bc67bc9f9e76681cabee56944284f4ec
bcolemutech <bcolemutech@hotmail.com>
2023-06-02 00:00:05 -0500

    Enum for biomes

---
+++ DolCon/Enums/Biome.cs
@@ -0,0 +1,18 @@
+﻿namespace DolCon.Enums;
+
+public enum Biome
+{
+    Marine,
+    HotDesert,
+    ColdDesert,
+    Savanna,
+    Grassland,
+    TropicalSeasonalForest,
+    TemperateDeciduousForest,
+    TropicalRainForest,
+    TemperateRainForest,
+    Taiga,
+    Tundra,
+    Glacier,
+    Wetland
+}
