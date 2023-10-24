900a6ff1bdbd7db9d07d029ba064a7591611440a
bcole <bcolemutech@gmail.com>
2023-10-23 22:05:36 -0500

    Event constructor

--- DolCon/Models/Event.cs
+++ DolCon/Models/Event.cs
@@ -1,6 +1,15 @@
-﻿namespace DolCon.Models;
+﻿using DolCon.Models.BaseTypes;
+
+namespace DolCon.Models;
 
 public class Event
 {
-    
+    public Event(Location? currentLocation, Cell currentCell)
+    {
+        Location = currentLocation;
+        Area = currentCell;
+    }
+
+    public Location? Location { get; set; }
+    public Cell Area { get; set; }
 }
