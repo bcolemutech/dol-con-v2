2d6bfaf57a50af297945582558699ebb44d9be7b
bcole <bcolemutech@gmail.com>
2023-10-27 00:52:38 -0500

    Reset and defaults for Scene object

--- DolCon/Models/Scene.cs
+++ DolCon/Models/Scene.cs
@@ -6,12 +6,22 @@ public class Scene
 {
     public MoveStatus MoveStatus { get; set; }
     public string Message { get; set; }
-    public bool IsCompleted { get; set; }
+    public bool IsCompleted { get; set; } = true;
     public SceneType Type { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public Dictionary<int, string> Selections { get; set; }
+    public Dictionary<int, string> Selections { get; set; } = new();
     public Service? SelectedService { get; set; }
     public int Selection { get; set; }
     public Location? Location { get; set; }
+
+    public void Reset()
+    {
+        Title = null;
+        Description = null;
+        Selections = new Dictionary<int, string>();
+        SelectedService = null;
+        Selection = 0;
+        Location = null;
+    }
 }
