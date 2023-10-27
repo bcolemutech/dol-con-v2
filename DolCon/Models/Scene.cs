24c7cfd428bfaabbca722223de66f7cba145fd20
bcole <bcolemutech@gmail.com>
2023-10-26 23:13:42 -0500

    Properties to drive scene rendering

--- DolCon/Models/Scene.cs
+++ DolCon/Models/Scene.cs
@@ -8,5 +8,10 @@ public class Scene
     public string Message { get; set; }
     public bool IsCompleted { get; set; }
     public SceneType Type { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public Dictionary<int, string> Selections { get; set; }
     public Service? SelectedService { get; set; }
+    public int Selection { get; set; }
+    public Location? Location { get; set; }
 }
