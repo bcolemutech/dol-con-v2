61e92da977ea0bd9d1acd766ab617b62b3b11542
bcole <bcolemutech@gmail.com>
2023-11-03 22:19:34 -0500

    Display items in a table

--- DolCon/Views/GameService.Inventory.cs
+++ DolCon/Views/GameService.Inventory.cs
@@ -1,21 +1,55 @@
-﻿using Spectre.Console;
+﻿using DolCon.Enums;
+using DolCon.Services;
+using Spectre.Console;
 
 namespace DolCon.Views;
 
 public partial class GameService
 {
+    private int _inventorySelected;
+
     private void RenderInventory()
     {
-        _display.Update(
-            new Columns(
-                new Panel(
-                    new Markup("[bold]Inventory[/]")
-                ),
-                new Panel(
-                    new Markup("[bold]Selected Details[/]")
-                )
-            )
-        );
+        if (_flow.Key is { Key: ConsoleKey.I })
+        {
+            _inventorySelected = 0;
+        }
+        
+        var table = new Table();
+        table.AddColumn("Select");
+        table.AddColumn("Name");
+        table.AddColumn("Description");
+        table.AddColumn("Rarity");
+        table.AddColumn("Tags");
+        table.AddColumn("Price");
+        var player = SaveGameService.Party.Players.First();
+        var i = 0;
+        foreach (var item in player.Inventory)
+        {
+            var selected = i == _inventorySelected ? "[green bold]X[/]" : "";
+            var rarity = item.Rarity switch
+            {
+                Rarity.Common => "[grey]Common[/]",
+                Rarity.Uncommon => "[green]Uncommon[/]",
+                Rarity.Rare => "[blue]Rare[/]",
+                Rarity.Epic => "[purple]Epic[/]",
+                Rarity.Legendary => "[gold1]Legendary[/]",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            var copper = item.Price % 100;
+            var silver = (item.Price / 100) % 100;
+            var gold = item.Price / 10000;
+            table.AddRow(
+                selected,
+                item.Name,
+                item.Description,
+                rarity,
+                item.Tags.Select(x => x.ToString()).Aggregate((x, y) => $"{x}, {y}") ?? "",
+                $"[bold gold1]{gold}[/]|[bold silver]{silver}[/]|[bold tan]{copper}[/]"
+            );
+            i++;
+        }
+        _display.Update(table);
         _ctx.Refresh();
         _controls.Update(
             new Panel(
